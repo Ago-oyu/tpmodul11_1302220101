fc7ca606075794086f5b3a84230352ac28abf0a7
Ago-oyu <95378690+Ago-oyu@users.noreply.github.com>
2024-05-05 22:08:17 +0700

    initial commit

---
+++ AljabarLibraries/Aljabar.cs
@@ -0,0 +1,25 @@
+﻿namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        public static double[] AkarPersamaanKuadrat(double[] Persamaan)
+        {
+            double a = Persamaan[0];
+            double b = Persamaan[1];
+            double c = Persamaan[2];
+
+            double x1 = (-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
+
+            return new double[] { x1, x2 };
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            return new double[] { a * a, 2 * a * b, b * b };
+        }
+    }
+}
---
+++ KalkulatorAljabar/Program.cs
@@ -0,0 +1,17 @@
+﻿using AljabarLibraries;
+
+namespace KalkulatorAljabar
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Akar persamaan dari x^2 - 3x - 10 adalah: ");
+            Console.WriteLine(String.Join(", ", Aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 })));
+
+
+            Console.Write("Hasil kuadrat dari (a + b)^2 adalah: ");
+            Console.WriteLine(String.Join(", ", Aljabar.HasilKuadrat(new double[] { 2, -3 })));
+        }
+    }
+}
